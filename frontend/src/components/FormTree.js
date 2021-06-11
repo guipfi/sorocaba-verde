a56fe46da72b812f369f1ee3be0fa22901a76643
Guilherme Fantini <guilhermepfantini@hotmail.com>
2021-06-11 17:46:32 -0300

    Tree register front progress

--- frontend/src/components/FormTree.js
+++ frontend/src/components/FormTree.js
@@ -1,50 +1,65 @@
-import "./styles/FormTree.css";
+import React, {useState} from 'react';
+
 import back from './assets/chevron-left-solid.svg';
 import axios from 'axios';
 
+import "./styles/FormTree.css";
+
 function FormTree(props) {
+
+    const [quantity, setQuantity] = useState(1);
     
     const handleSubmit = (event) => {
         
         event.preventDefault();
 
-        const imagesQtd = event.target[3].files.length;
+        let img_index
+        if(quantity > 1) {
+            img_index = 4;
+        } else {
+            img_index = 6
+        }
+        const imagesQtd = event.target[img_index].files.length; 
         let photosURL = [];
 
         for(var i = 0; i < imagesQtd; i++)
-            photosURL.push(event.target[3].files[i].name)
+            photosURL.push(event.target[img_index].files[i].name)
 
-        axios.get('http://localhost:8082/api/users/isLogged',{withCredentials: true, credentials: 'include'})
+        console.log("aqui");
+        axios.get('http://localhost:8082/api/admin/isLogged',{withCredentials: true, credentials: 'include'})
         .then(res=>{
+            console.log(res);
             if(res.data.code === 1){
                 
                 var bodyFormData = new FormData();
-                bodyFormData.append('address', event.target[0].value);
+                bodyFormData.append('address', props.address);
                 bodyFormData.append('lat', props.lat);
                 bodyFormData.append('lng', props.lng);
-                bodyFormData.append('type', event.target[1].value)
-                bodyFormData.append('description', event.target[2].value);
-                bodyFormData.append('solicitator', res.data.user._id);
-                bodyFormData.append('photosURL', event.target[3].files);
-                                
-                const tree = {
-                    "address": event.target[0].value,
-                    "lat": props.lat,
-                    "lng": props.lng,
-                    "type": event.target[1].value,
-                    "description": event.target[2].value,
-                    "solicitator": res.data.user._id,
-                    "photosURL": event.target[3].files
+                bodyFormData.append('quantity', event.target[1].value);
+                bodyFormData.append('name', event.target[2].value);
+                bodyFormData.append('photosURL', photosURL);
+
+                if(quantity > 1) {
+                    bodyFormData.append('description', event.target[3].value);
+                } else {
+                    bodyFormData.append('height', event.target[3].value);
+                    bodyFormData.append('age', event.target[4].value);
+                    bodyFormData.append('description', event.target[5].value);
                 }
-    
-                axios.post('http://localhost:8082/api/tree/new', bodyFormData)
+                           
+                console.log("hello");
+                axios.post('http://localhost:8082/api/trees/new', bodyFormData)
                 .then(res =>{
                     if(res.status === 200){
-                        window.location.href = "/" 
+                        window.location.href = "/sistema/home" 
+                    } else {
+                        console.log(res.statusText);
                     }
                 })
             }
-        })
+        }).catch((error) => {
+            console.log(error);
+        });
     }
     
     return(
@@ -54,28 +69,41 @@ function FormTree(props) {
                 <div className="button-back" style={{display: 'inline-block', marginRight: '0.5rem'}}>
                     <a href="/sistema/home"><img src={back} alt="Voltar"/></a>
                 </div>
-                Nova Árvore'
+                Nova Árvore
             </h3>
 
             <form onSubmit={handleSubmit}>
                 <label htmlFor="address">Endereço</label>
-                <input type="address" placeholder="Digite seu endereço..." value={props.address} disabled></input>
+                <input type="text" value={props.address} disabled required></input>
+
+                <label htmlFor="quantity">Quantidade</label>
+                <input type="number" placeholder="Quantidade de árvores..." value={quantity} onChange={(e) => setQuantity(e.target.value)} min="1" required></input>
                 
-                <label htmlFor="type">Tipo de solicitação</label>
-                <select name="type" className="type">
-                    <option value="Corte">Corte</option>
-                    <option value="Poda">Poda</option>
-                    <option value="Substituição">Substituição</option>
-                </select>
-                
-                <label htmlFor="description">Descrição</label>
-                <textarea placeholder="Digite uma descrição..."></textarea>
+                <label htmlFor="tree-infos">Informações técnicas</label>
+                <div className="tree-infos">
+                    <label htmlFor="name">Nome</label>
+                    <input type="text" placeholder="Nome da árvore..." required></input>
+                    {quantity == 1 ? (
+                        <div className="tree-attributes">
+                            <div>
+                                <label htmlFor="height">Altura (em metros)</label>
+                                <input type="number" step=".01" min="0.1" placeholder="1,7m" required={quantity == 1 ? true : false}></input> 
+                            </div>
+                            <div>
+                                <label htmlFor="age">Idade (em anos)</label>
+                                <input type="number" step="1" min="0" placeholder="5" required={quantity == 1 ? true : false}></input>   
+                            </div>
+                        </div>
+                    ) : (null) }
+                    <label htmlFor="description">Descrição</label>
+                    <textarea placeholder="Digite uma descrição..." required></textarea>
+                </div>
                 
                 <label>Fotos</label>
                 <input className="photo-input" type="file" placeholder="Adicionar Imagem" multiple></input>
 
                 <div id="button-container">
-                    <button onClick={() => window.location.href="/"} id="cancel-button">Cancelar</button>
+                    <button onClick={() => window.location.href="/sistema/home"} id="cancel-button">Cancelar</button>
                     <button type="submit">Confirmar</button>
                 </div>
             </form>
